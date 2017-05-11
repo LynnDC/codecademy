f78ea3fe7fb064ff255443aca6aaacbcc004caa3
Lynn Chen <lynn.d.chen@gmail.com>
2017-05-11 15:37:54 -0700

    Update PyUnit34567.py

--- PyUnit34567.py
+++ PyUnit34567.py
@@ -38,3 +38,28 @@ def rental_car_cost(days):
 def trip_cost(city,days,spending_money):
     return rental_car_cost(days) + hotel_cost(days) + plane_ride_cost(city) + spending_money
 print trip_cost("Los Angeles", 5, 600)
+
+#Unit 5: A Day at the Supermarket - part 12
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+    
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+def compute_bill(food):
+    total = 0
+    for item in food:
+        if stock[item] > 0:
+            total += prices[item]
+            stock[item] = stock[item] -1
+    return total
