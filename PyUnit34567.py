b47b72560868ce9d1bb7038cbcab7be17eee8621
Lynn Chen <lynn.d.chen@gmail.com>
2017-05-11 15:49:08 -0700

    Update PyUnit34567.py

--- PyUnit34567.py
+++ PyUnit34567.py
@@ -63,3 +63,55 @@ def compute_bill(food):
             total += prices[item]
             stock[item] = stock[item] -1
     return total
+#Unit 6: Student Becomes the Teacher - part 9
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+# Add your function below!
+def average(numbers):
+    total = sum(numbers)
+    average = float(total)/len(numbers)
+    return average
+    
+def get_average(student):
+    homework = average(student["homework"])
+    quizzes = average(student["quizzes"])
+    tests = average(student["tests"])
+    return 0.1 * average(student["homework"]) + 0.3 * average(student["quizzes"]) + 0.6 * average(student["tests"]);
+    
+def get_letter_grade(score):
+    if score >= 90:
+        return "A"
+    elif 80 <= score:
+        return "B"
+    elif 70 <= score:
+        return "C"
+    elif 60 <= score:
+        return "D"
+    else: 
+        return "F"
+    return get_letter_grade(get_average(lloyd))
+    
+def get_class_average(students):
+    results = []
+    for student in students:
+        results.append(get_average(student))
+    return average(results)
+print get_class_average(students)
+print get_letter_grade(get_class_average(students))
